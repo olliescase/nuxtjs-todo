c033f5a8dd8d29bb7c7c7464ac3ccb26441eb3df
Oliver Scase <oliver@lagerroos.nu>
2022-06-16 22:08:14 +0200

    Initial commit for todo

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>TODO</h1>
+    <p>This page is rendered on the <strong>{{ rendering }}</strong></p>
+    <p v-if="rendering === 'server'">
+      First load or hard refresh done on server side
+    </p>
+    <p v-if="rendering === 'client'">
+      Navigation is done on the client side
+    </p>
+    <ul>
+      <li>Refersh page for server-side loading</li>
+      <li>Click links to see client-side rendering</li>
+    </ul>
+
+    <NuxtLink to="/about">About Page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    console.log(this.$nuxt.$route);
+
+    this.rendering = process.server ? "server" : "client";
+  },
+}
+</script>
